f0023d96892c5f0162de6feeff270d8b5c7f63da
RohanKhatua <rohan.khatua2021@vistudent.ac.in>
2023-04-02 10:04:49 +0530

    add: terminal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const colors = {
+  green: "#00ff00",
+  white: "#ffffff",
+};
 
 function App() {
+  const [input, setInput] = useState("");
+  const [history, setHistory] = useState([]);
+  const [output, setOutput] = useState("");
+
+  const handleInputChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    switch (input.toLowerCase()) {
+      case "app":
+        setHistory([
+          ...history,
+          { command: "app", output: "You have entered the App command." },
+        ]);
+        break;
+      case "web":
+        setHistory([
+          ...history,
+          { command: "web", output: "You have entered the Web command." },
+        ]);
+        break;
+      case "research":
+        setHistory([
+          ...history,
+          {
+            command: "research",
+            output: "You have entered the Research command.",
+          },
+        ]);
+        break;
+      case "management":
+        setHistory([
+          ...history,
+          {
+            command: "management",
+            output: "You have entered the Management command.",
+          },
+        ]);
+        break;
+      case "design":
+        setHistory([
+          ...history,
+          {
+            command: "design",
+            output: "You have entered the Design command.",
+          },
+        ]);
+        break;
+      case "cc":
+        setHistory([
+          ...history,
+          {
+            command: "cc",
+            output: "You have entered the CC command.",
+          },
+        ]);
+        break;
+      case "help":
+        setHistory([
+          ...history,
+          {
+            command: "help",
+            output:
+              "Available commands: app, web, research, management, design, cc",
+          },
+        ]);
+        break;
+      default:
+        setHistory([
+          ...history,
+          {
+            command: input,
+            output: "Command not found. Type 'help' for a list of commands.",
+          },
+        ]);
+        break;
+    }
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {history.map((item, index) => (
+        <div key={index}>
+          <span>user@acm-mainframe${item.command}</span>
+          <br />
+          <span>{item.output}</span>
+          <br />
+        </div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>user@acm-mainframe$</span>
+        </label>
+        <input
+          className="command-input"
+          type="text"
+          value={input}
+          onChange={handleInputChange}
+        />
+      </form>
     </div>
   );
 }
